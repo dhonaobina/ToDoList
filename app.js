926a0cb980094c87de113cb5c6e2a45893545af3
dhonaobina <dhona.obina@outlook.com>
2024-03-06 21:08:42 +1300

    created for loops for the add task button with its status and button to click when task pending to completed

--- app.js
+++ app.js
@@ -11,7 +11,31 @@ function addItem() {
     else
     {
         document.getElementById('taskList').appendChild(listItem);
+        var statusbtn = document.createElement("button");
+        var btnText = document.createTextNode("Pending");
+        statusbtn.className = "status";
+        statusbtn.appendChild(btnText);
+        tasks[i].appendChild(statusbtn);
+        tasks[i].addEventListener('click', function (ev) {ev.target.innerHTML = "Completed"; }, false);
+        i++;
         document.getElementById("taskDetails").value = "";
         document.getElementById("txtout").innerHTML = "";
     }
+}
+
+var tasks = document.getElementsByTagName("LI");
+for (var i=0; i < tasks.length; i++) 
+{
+    var statusbtn = document.createElement("button");
+    var btnText = document.createTextNode("Pending");
+    var txt = "btn";
+    statusbtn.className = "status";
+    statusbtn.appendChild(btnText);
+    tasks[i].appendChild(statusbtn);
+}
+
+var btns = document.getElementsByClassName("status");
+for (j=0; j<=btns.length; j++)
+{
+    btns[j].addEventListener('click', function (ev) {ev.target.innerHTML = "Completed";}, false);
 }
